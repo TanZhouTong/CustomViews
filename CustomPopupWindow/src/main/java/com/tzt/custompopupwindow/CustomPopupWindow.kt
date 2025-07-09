e35feafc1a44b11121e8de89580b537c8a1c3aaa
Jent <1499151338@qq.com>
2025-07-10 01:39:19 +0800

    1.popupwindow自适应内容宽度实现

--- CustomPopupWindow/src/main/java/com/tzt/custompopupwindow/CustomPopupWindow.kt
+++ CustomPopupWindow/src/main/java/com/tzt/custompopupwindow/CustomPopupWindow.kt
@@ -2,9 +2,12 @@ package com.tzt.custompopupwindow
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.util.Log
 import android.util.TypedValue
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.PopupWindow
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -26,6 +29,17 @@ class CustomPopupWindow(
             150f,
             context.resources.displayMetrics
         ).toInt()
+        /**
+         * popupWindow必须手动设置其宽高，
+         * 1.如果设置了宽高就采用这里的宽高，子根布局的设置可能会忽略以及出现未知异常现象，
+         * 2.如果popupWindow要适配子布局大小，可以设置为ViewGroup.LayoutParams.WRAP_CONTENT，并且要提前测量子布局的大小，在show之前应用到popupWindow中
+         * */
+        /*width = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            200f,
+            context.resources.displayMetrics
+        ).toInt()*/
+        width = ViewGroup.LayoutParams.WRAP_CONTENT
         isFocusable = true
         isOutsideTouchable = false
         setBackgroundDrawable(context.getDrawable(R.drawable.popup_background))
@@ -45,7 +59,13 @@ class CustomPopupWindow(
     }
 
     override fun showAsDropDown(anchor: View?) {
-        val xOffset = (width - (anchor?.width ?: 0)) shr (1)
-        super.showAsDropDown(anchor, xOffset, 2)
+        contentView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        val xOffset = (contentView.measuredWidth - (anchor?.width ?: 0)) shr (1)
+        Log.d("tzt", "cur_width:$width  anchor_width:${anchor?.width}")
+        Log.d("tzt", "cur_width:${contentView.measuredWidth}  anchor_width:${anchor?.width}")
+        Log.d("tzt", "xOffset:$xOffset")
+        super.showAsDropDown(anchor, -xOffset, 20)
+        /*super.showAsDropDown(anchor, 0, 0, Gravity.CENTER_VERTICAL)*/
+        /*super.showAsDropDown(anchor)*/
     }
 }
